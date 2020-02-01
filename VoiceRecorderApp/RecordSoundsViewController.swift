fb22e6171daa25e47837b53b1ad38c5560614bc7
ramon baeza yepez <By_ramon@outlook.com>
2020-02-01 13:45:23 -0600

    fin clase uno

--- VoiceRecorderApp/RecordSoundsViewController.swift
+++ VoiceRecorderApp/RecordSoundsViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
+    //MARK: Oulets
     @IBOutlet weak var lblRecording: UILabel!
     @IBOutlet weak var btnRecord: UIButton!
     @IBOutlet weak var btnStopRecord: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -33,14 +38,29 @@ class RecordSoundsViewController: UIViewController {
         btnStopRecord.isEnabled = true
         btnRecord.isEnabled = !btnStopRecord.isEnabled
         let text = "Recording in progress"
-        print(text)
         lblRecording.text = text
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        print(filePath)
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+         //audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     @IBAction func stopRecording(_ sender: Any) {
         btnRecord.isEnabled = true
         btnStopRecord.isEnabled = !btnRecord.isEnabled
         let text = "Tap to Record"
-        print(text)
         lblRecording.text = text
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
     }
 }
