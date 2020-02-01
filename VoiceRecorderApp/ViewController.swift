523e9cab4056878c5c6348b17d19c21459d77fff
ramon baeza yepez <By_ramon@outlook.com>
2020-02-01 11:17:47 -0600

    Initial Commit

---
+++ VoiceRecorderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceRecorderApp
+//
+//  Created by ByRamon on 2/1/20.
+//  Copyright © 2020 ByRamon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
